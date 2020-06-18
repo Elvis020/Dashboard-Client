ecad7972bee6a6af89c4f4b94339c80fa2b00a2b
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-06-18 23:36:30 +0000

    Css-added but needs update

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -17,6 +17,7 @@ import {
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  hide = true;
   emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
   signUpForm: FormGroup;
   email = '';
